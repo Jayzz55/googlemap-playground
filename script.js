ba9e4edb45f8a03809a5ee6781e7193ae0c47706
Jaya Wijono from Dot Kingdom <jayzzwijono@yahoo.com>
2015-08-02 15:40:03 +1000

    implement marker events

--- script.js
+++ script.js
@@ -5,11 +5,23 @@
   element = document.getElementById('map-canvas'),
   // map
   map = mapster.create(element, options);
-  map.zoom(18);
-  map._on('click',function(e){
-    alert('click');
-    console.log(e);
-    console.log(this);
+ 
+  var marker1 = map.addMarker({
+    lat: 37.791350,
+    lng: -122.435883,
+    content: '<div style="color: #f00;">I like food</div>',
+    event: {
+      name: 'click',
+      callback: function() {
+        alert('hear you');
+      }
+    }
   });
-  
+
+  var marker2 = map.addMarker({
+    lat: 37.781350,
+    lng: -122.485883,
+    content: 'I like rice'
+  });  
+
 }(window, window.Mapster));
